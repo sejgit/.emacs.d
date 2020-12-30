c2616b97a593bb009bc1abff917e27f8adbbb827
sejgit <sejgit@github.com>
2020-12-30 18:01:46 -0500

    massive speed-up by tuning off diff-hl-flydiff-mode

--- init.el
+++ init.el
@@ -2090,7 +2090,7 @@ Return its absolute path.  Otherwise, return nil."
          (dired-mode . diff-hl-dired-mode))
   :config
   ;; Highlight on-the-fly
-  (diff-hl-flydiff-mode 1)
+;;  (diff-hl-flydiff-mode 1)
 
   ;; Set fringe style
   (setq-default fringes-outside-margins t)
@@ -2283,7 +2283,8 @@ Return its absolute path.  Otherwise, return nil."
          )
   :commands (lsp lsp-deferred)
   :config
-  (setq lsp-keymap-prefix "s-l"))
+  (setq lsp-keymap-prefix "s-l")
+  (setq lsp-prefer-flymake t)  )
 
 
 ;;;;; lsp-ui
@@ -3415,11 +3416,13 @@ If the region is active and option `transient-mark-mode' is on, call
   :straight (cc-mode :type built-in)
   :bind (:map c-mode-base-map
               ("C-c c" . compile))
-  :hook ((c-mode-common . flycheck-mode)
+  :hook (
+         ;;(c-mode-common . flycheck-mode)
          (c-mode-common . (lambda ()
                             (c-set-style "bsd")
                             (setq tab-width 4)
-                            (setq c-basic-offset 4))))
+                            (setq c-basic-offset 4)
+                            (flycheck-mode nil))))
   :init
   ;; Set the default formatting styles for various C based modes
   (setq c-default-style
@@ -3432,10 +3435,73 @@ If the region is active and option `transient-mark-mode' is on, call
 ;; - used for both lsp & eglot so no :if statement
 ;; - [[https://github.com/MaskRay/ccls/wiki/lsp-mode][emacs-ccls]]
 (use-package ccls
-  ;; :init (add-to-list 'auto-mode-alist '("\\.ino\\'" . c++-mode))
+  :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda () (require 'ccls) (lsp)))
   :config
-  '(ccls-initialization-options (quote (compilationDatabaseDirectory :build))))
+ (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
+ (setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)
+ (setq ccls-sem-highlight-method 'font-lock)
+ ;; alternatively, (setq ccls-sem-highlight-method 'overlay)
 
+ ;; For rainbow semantic highlighting
+ (ccls-use-default-rainbow-sem-highlight)
+
+;; direct callers
+;; (lsp-find-custom "$ccls/call")
+;; callers up to 2 levels
+;; (lsp-find-custom "$ccls/call" '(:levels 2))
+;; direct callees
+;; (lsp-find-custom "$ccls/call" '(:callee t))
+
+;; (lsp-find-custom "$ccls/vars")
+; Use lsp-goto-implementation or lsp-ui-peek-find-implementation (textDocument/implementation) for derived types/functions
+; $ccls/inheritance is more general
+
+;; Alternatively, use lsp-ui-peek interface
+;; (lsp-ui-peek-find-custom "$ccls/call")
+;; (lsp-ui-peek-find-custom "$ccls/call" '(:callee t))
+
+(defun ccls/callee () (interactive) (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
+(defun ccls/caller () (interactive) (lsp-ui-peek-find-custom "$ccls/call"))
+(defun ccls/vars (kind) (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
+(defun ccls/base (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
+(defun ccls/derived (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
+(defun ccls/member (kind) (interactive) (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
+
+;; ;; References w/ Role::Role
+(defun ccls/references-read () (interactive)
+  (lsp-ui-peek-find-custom "textDocument/references"
+    (plist-put (lsp--text-document-position-params) :role 8)))
+
+;; References w/ Role::Write
+(defun ccls/references-write ()
+  (interactive)
+  (lsp-ui-peek-find-custom "textDocument/references"
+   (plist-put (lsp--text-document-position-params) :role 16)))
+
+;; References w/ Role::Dynamic bit (macro expansions)
+(defun ccls/references-macro () (interactive)
+  (lsp-ui-peek-find-custom "textDocument/references"
+   (plist-put (lsp--text-document-position-params) :role 64)))
+
+;; References w/o Role::Call bit (e.g. where functions are taken addresses)
+(defun ccls/references-not-call () (interactive)
+  (lsp-ui-peek-find-custom "textDocument/references"
+   (plist-put (lsp--text-document-position-params) :excludeRole 32)))
+
+;; ccls/vars ccls/base ccls/derived ccls/members have a parameter while others are interactive.
+;; (ccls/base 1) direct bases
+;; (ccls/derived 1) direct derived
+;; (ccls/member 2) => 2 (Type) => nested classes / types in a namespace
+;; (ccls/member 3) => 3 (Func) => member functions / functions in a namespace
+;; (ccls/member 0) => member variables / variables in a namespace
+;; (ccls/vars 1) => field
+;; (ccls/vars 2) => local variable
+;; (ccls/vars 3) => field or local variable. 3 = 1 | 2
+;; (ccls/vars 4) => parameter
+
+;; References whose filenames are under this project
+;; (lsp-ui-peek-find-references nil (list :folders (vector (projectile-project-root))))
+)
 
 ;;;;; modern-cpp-font-lock
 ;; - Syntax highlighting support for "Modern C++" - until C++20 and Technical Specification
