6bc6865a72920f54ad77843596f55028eb512680
sejgit <sejgit@github.com>
2021-07-07 18:44:27 -0400

    spectrum consult added

--- init.el
+++ init.el
@@ -740,13 +740,6 @@ Return its absolute path.  Otherwise, return nil."
           ("C-h M" . helpful-macro))  )
 
 
-;;;;; discover
-;; - discover more of emacs using context menus
-;; - [[https://github.com/mickeynp/discover.el][discover github]]
-(use-package discover
-  :hook (emacs-startup . global-discover-mode))
-
-
 ;;; user interface
 ;;;; themes
 ;;;;; modus themes
@@ -1316,7 +1309,11 @@ Return its absolute path.  Otherwise, return nil."
 ;; - [[https://github.com/raxod502/selectrum#what-is-it][selectrum]]
 (use-package selectrum
   :config
-  (selectrum-mode +1))
+  (selectrum-mode +1)
+
+  (advice-add 'slime-display-or-scroll-completions :around
+             (defun my--slime-completion-in-region (_ completions start end)
+               (completion-in-region start end completions))))
 
 
 ;;;;; selectrum-precient
@@ -1327,17 +1324,175 @@ Return its absolute path.  Otherwise, return nil."
   (selectrum-prescient-mode +1)
   (prescient-persist-mode +1))
 
+;;;;; marginalia
+;; Enable richer annotations using the Marginalia package
+;; [[https://github.com/minad/marginalia][marginalia]]
+(use-package marginalia
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
 
-;;;;; ctrlf
-;; - intuitive and efficient solution for single-buffer text search
-;; - [[https://github.com/raxod502/ctrlf][ctrlf]]
-(use-package ctrlf
-  :bind
-  (("C-s" . ctrlf-forward-fuzzy)
-   ("C-r" . ctrlf-backward-fuzzy)
-   ("M-s" . ctrlf-forward-symbol-at-point))
+  ;; The :init configuration is always executed (Not lazy!)
   :init
-  (ctrlf-mode +1))
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
+
+;;;;; embark
+;; acting on targets
+;; [[https://github.com/oantolin/embark/][embark]]
+(use-package embark
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+  (setq embark-action-indicator
+      (lambda (map _target)
+        (which-key--show-keymap "Embark" map nil nil 'no-paging)
+        #'which-key--hide-popup-ignore-command)
+      embark-become-indicator embark-action-indicator)
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :after (embark consult)
+  :demand t ; only necessary if you have the hook below
+  ;; if you want to have consult previews as you move around an
+  ;; auto-updating embark collect buffer
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+
+;;;;; selectrum-consult
+;; - complementary to selectrum
+;; - [[https://github.com/minad/consult][consult]]
+(use-package consult
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind (;; C-c bindings (mode-specific-map)
+         ("C-c h" . consult-history)
+         ("C-c m" . consult-mode-command)
+         ("C-c b" . consult-bookmark)
+         ("C-c k" . consult-kmacro)
+         ;; C-x bindings (ctl-x-map)
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ("<help> a" . consult-apropos)            ;; orig. apropos-command
+         ;; M-g bindings (goto-map)
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-project-imenu)
+         ;; M-s bindings (search-map)
+         ("M-s f" . consult-find)
+         ("M-s L" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s m" . consult-multi-occur)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch)
+         ("C-s" . consult-isearch)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch)                 ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch)               ;; orig. isearch-edit-string
+         ("M-s l" . consult-line))                 ;; needed by consult-line to detect isearch
+
+  ;; Enable automatic preview at point in the *Completions* buffer.
+  ;; This is relevant when you use the default completion UI,
+  ;; and not necessary for Vertico, Selectrum, etc.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Optionally replace `completing-read-multiple' with an enhanced version.
+  (advice-add #'completing-read-multiple :override #'consult-completing-read-multiple)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key (kbd "M-."))
+  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme
+   :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-file consult--source-project-file consult--source-bookmark
+   :preview-key (kbd "M-."))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; (kbd "C-+")
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; Optionally configure a function which returns the project root directory.
+  ;; There are multiple reasonable alternatives to chose from.
+;;;;;;; 1. project.el (project-roots)
+  ;; (setq consult-project-root-function
+  ;;       (lambda ()
+  ;;         (when-let (project (project-current))
+  ;;          (car (project-roots project)))))
+;;;;;;; 2. projectile.el (projectile-project-root)
+   (autoload 'projectile-project-root "projectile")
+   (setq consult-project-root-function #'projectile-project-root)
+;;;;;;; 3. vc.el (vc-root-dir)
+  ;; (setq consult-project-root-function #'vc-root-dir)
+;;;;;;; 4. locate-dominating-file
+  ;; (setq consult-project-root-function (lambda () (locate-dominating-file "." ".git")))
+)
 
 
 ;;;;; bookmark+
@@ -1465,6 +1620,7 @@ Return its absolute path.  Otherwise, return nil."
           ("C-c n" . crux-cleanup-buffer-or-region)
           ("C-c u" . crux-view-url)
           ("s-k" . crux-duplicate-current-line-or-region)
+          ("C-x C-r" . crux-recentf-find-file)
           ("C-c C-k" . crux-duplicate-current-line-or-region)
           ("C-c M-k" . crux-duplicate-and-comment-current-line-or-region)
           ([remap kill-whole-line] . crux-kill-whole-line)
@@ -4979,32 +5135,78 @@ used as `:filter-return' advice to `eshell-ls-decorated-name'."
   :straight (:type built-in)
   :bind ("C-c s C-e" . sej/erc-dwim)
   :config
+  ;; from [[https://www.emacswiki.org/emacs/ErcSSL][emacswiki.org erc-tls hack]]
+    ;; erc hack for gnutls for client cert.
+    (defvar *uconf/erc-certs* nil
+      "erc client certs used by gnutls package for :keylist.")
+
+    ;; copied from the gnutls lib but set :keylist to client certs.
+    ;; this function is called from `open-network-stream' with :type tls.
+    (defun uconf/open-gnutls-stream (name buffer host service &optional nowait)
+      (let ((process (open-network-stream
+                      name buffer host service
+                      :nowait nowait
+                      :tls-parameters
+                      (and nowait
+                           (cons 'gnutls-x509pki
+                                 (gnutls-boot-parameters
+                                  :type 'gnutls-x509pki
+                                  :keylist *uconf/erc-certs* ;;added parameter to pass the cert.
+                                  :hostname (puny-encode-domain host)))))))
+        (if nowait
+            process
+          (gnutls-negotiate :process process
+                            :type 'gnutls-x509pki
+                            :keylist *uconf/erc-certs* ;;added parameter to pass the cert.
+                            :hostname (puny-encode-domain host)))))
+
+    ;; only set the global variable when used from `erc-tls'.
+    (defun uconf/erc-open-tls-stream (name buffer host port)
+      (unwind-protect
+          (progn
+            (setq *uconf/erc-certs* sej-tls-certfile)
+            (open-network-stream name buffer host port
+                                 :nowait t
+                                 :type 'tls))
+        (setq *uconf/erc-certs* nil)))
+
+    (advice-add 'open-gnutls-stream :override #'uconf/open-gnutls-stream)
+    (advice-add 'erc-open-tls-stream :override #'uconf/erc-open-tls-stream)
+
   (setq erc-prompt-for-password nil)
 
   (defun sej/erc-dwim ()
     "Switch to latest `erc' buffer or log in."
     (interactive)
-    (let* ((irc "irc.freenode.net")
+    (let* ((irc "irc.libera.chat")
            (nick sej-irc-nick)
            (pass sej-irc-pass)
            (bufs (erc-buffer-list)))
       (if bufs
           (erc-track-switch-buffer 1)
-        (erc :server irc
-             :nick nick
-             :password pass
-             :full-name nick))))
+         (erc-tls :server irc
+                  :port 6697
+                  :nick nick
+                  :password pass
+                  :full-name nick
+                  )
+  ;;       (erc :server irc
+    ;;          :port 6697
+      ;;        :nick nick
+        ;;      :password pass
+          ;;    :full-name nick)
+        )))
 
   ;; Options
 
-  ;; Join the #emacs and #erc channels whenever connecting to Freenode.
-  (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#erc" "#emacs-beginners")))
+  ;; Join the #emacs and #erc channels whenever connecting to libera.
+  (setq erc-autojoin-channels-alist '(("libera.net" "#emacs" "#erc" "#emacs-chat" "#emacs-til")))
 
   ;; If non, nil, this is a list of IRC message types to hide, e.g.:
   (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
   ;; If non, nil, this is a list of IRC networks and message types to hide, e.g.:
-  (setq erc-network-hide-list '(("freenode" "JOIN" "PART" "QUIT")
+  (setq erc-network-hide-list '(("JOIN" "PART" "QUIT")
                                ("OFTC" "JOIN" "PART")))
 
   ;; If non, nil, this is a list of IRC channels and message types to hide, e.g.:
