69df9c84360cb7578284c1aa4ee5667e3609eff6
sejgit <sejgit@users.noreply.github.com>
2023-07-31 21:20:32 -0400

    undelete-frame-mode t  C-x 5 u

--- init.el
+++ init.el
@@ -1035,6 +1035,7 @@ Return its absolute path.  Otherwise, return nil."
         window-divider-default-right-width 1
         frame-title-format '("Emacs - %b")
         icon-title-format frame-title-format
+        undelete-frame-mode t
         )
 
   (blink-cursor-mode -1)
