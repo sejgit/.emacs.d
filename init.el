a84f631fbc88767e7c3a4ae2139c40a90cdb2ad2
sejgit <5984155+sejgit@users.noreply.github.com>
2024-03-16 12:46:38 -0400

    whitespace-mode better settings

--- init.el
+++ init.el
@@ -523,6 +523,50 @@
   (prefer-coding-system 'utf-8) ; with sugar on top
 
   :init
+;;;;;; whitespace
+  ;; Define the whitespace style.
+  (setq-default whitespace-style
+                '(face spaces empty tabs newline trailing space-mark tab-mark newline-mark))
+
+  ;; Whitespace color corrections.
+  (require 'color)
+  (let* ((ws-lighten 10) ;; Amount in percentage to lighten up.
+         (ws-color (color-lighten-name "#5C5655" ws-lighten)))
+    (custom-set-faces
+     `(whitespace-newline                ((t (:foreground ,ws-color))))
+     `(whitespace-missing-newline-at-eof ((t (:foreground ,ws-color))))
+     `(whitespace-space                  ((t (:foreground ,ws-color))))
+     `(whitespace-space-after-tab        ((t (:foreground ,ws-color))))
+     `(whitespace-space-before-tab       ((t (:foreground ,ws-color))))
+     `(whitespace-tab                    ((t (:foreground ,ws-color))))
+     `(whitespace-trailing               ((t (:foreground ,ws-color))))))
+
+  ;; Make these characters represent whitespace.
+  (setq-default whitespace-display-mappings
+                '(
+                  ;; space -> · else .
+                  (space-mark 32 [183] [46])
+                  ;; new line -> ¬ else $
+                  (newline-mark ?\n [172 ?\n] [36 ?\n])
+                  ;; carriage return (Windows) -> ¶ else #
+                  (newline-mark ?\r [182] [35])
+                  ;; tabs -> » else >
+                  (tab-mark ?\t [187 ?\t] [62 ?\t])))
+
+  ;; Don't enable whitespace for.
+  (setq-default whitespace-global-modes
+                '(not shell-mode
+                    help-mode
+                    magit-mode
+                    magit-diff-mode
+                    ibuffer-mode
+                    dired-mode
+                    occur-mode))
+
+  ;; set whitespace actions.
+  (setq-default whitespace-action
+                '(cleanup auto-cleanup))
+
 ;;;;;; completion
   ;; Add prompt indicator to `completing-read-multiple'.
   ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
