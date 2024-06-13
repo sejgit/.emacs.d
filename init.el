479359c58509aa6a1eaecfb3bdc8ec862fb89edf
sejgit <5984155+sejgit@users.noreply.github.com>
2024-06-13 19:28:28 -0400

    codeium & change cape keybind

--- init.el
+++ init.el
@@ -1838,23 +1838,23 @@ Useful if you want a more robust view into the recommend candidates."
   :demand t
   ;; Bind dedicated completion commands
   ;; Alternative prefix keys: C-c p, M-p, M-+, ...
-  :bind* (("C-c p p" . completion-at-point) ;; capf
-          ("C-c p t" . complete-tag)        ;; etags
-          ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-          ("C-c p h" . cape-history)
-          ("C-c p f" . cape-file)
-          ("C-c p k" . cape-keyword)
-          ("C-c p s" . cape-elisp-symbol)
-          ("C-c p e" . cape-elisp-block)
-          ("C-c p a" . cape-abbrev)
-          ("C-c p i" . cape-dict)
-          ("C-c p l" . cape-line)
-          ("C-c p w" . cape-dict)
-          ("C-c p \\" . cape-tex)
-          ("C-c p _" . cape-tex)
-          ("C-c p ^" . cape-tex)
-          ("C-c p &" . cape-sgml)
-          ("C-c p r" . cape-rfc1345))
+  :bind* (("A-/ p" . completion-at-point) ;; capf
+          ("A-/ t" . complete-tag)        ;; etags
+          ("A-/ d" . cape-dabbrev)        ;; or dabbrev-completion
+          ("A-/ h" . cape-history)
+          ("A-/ f" . cape-file)
+          ("A-/ k" . cape-keyword)
+          ("A-/ s" . cape-elisp-symbol)
+          ("A-/ e" . cape-elisp-block)
+          ("A-/ a" . cape-abbrev)
+          ("A-/ i" . cape-dict)
+          ("A-/ l" . cape-line)
+          ("A-/ w" . cape-dict)
+          ("A-/ \\" . cape-tex)
+          ("A-/ _" . cape-tex)
+          ("A-/ ^" . cape-tex)
+          ("A-/ &" . cape-sgml)
+          ("A-/ r" . cape-rfc1345))
   :init
   ;; completion functions takes precedence over the global list.
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
@@ -5934,31 +5934,69 @@ defined keys follow the pattern of <PREFIX> <KEY>.")
         (add-to-list 'tmr-timer-finished-functions #'sej/osx-alert-tmr)
         (delete 'tmr-notification-notify tmr-timer-finished-functions))))
 
+;;;; Codeium
+;; completion for codeium
+;; [[https://github.com/Exafunction/codeium.el][Codeium]]
+(use-package codeium
+    ;; if you use straight
+    :straight '(:type git :host github :repo "Exafunction/codeium.el")
+    ;; otherwise, make sure that the codeium.el file is on load-path
+    :bind ("A-/ c" . codeium-completion-at-point)
+    :init
+    ;; use globally
+    (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
+    ;; or on a hook
+    ;; (add-hook 'python-base-mode-hook
+    ;;     (lambda ()
+    ;;         (add-to-list 'completion-at-point-functions '(codeium-completion-at-point))))
 
-;;; ChatGPT
-;;;;; openai
-;; backend for chatgpt & codegpt
-;; [[https://github.com/emacs-openai/openai][emacs-openai]]
-(use-package openai
-  :straight (openai :type git :host github :repo "emacs-openai/openai")
-  :config
-  (setq openai-key sej-chatgpt-key
-        openai-user sej-chatgpt-user))
+    ;; if you want multiple completion backends, use cape (https://github.com/minad/cape):
+    (add-hook 'python-mode-hook
+        (lambda ()
+            (setq-local completion-at-point-functions
+                (list (cape-super-capf #'codeium-completion-at-point #'lsp-completion-at-point)))))
+    ;; an async company-backend is coming soon!
 
-(use-package chatgpt
-  ;; Emacs code extension to use official OpenAI API
-  ;; [[https://github.com/emacs-openai/chatgpt][emacs chatgpt]]
-  :straight (chatgpt :type git :host github :repo "emacs-openai/chatgpt")
-  :commands chatgpt
-  :config
-  (setq openai--show-log t))
+    ;; codeium-completion-at-point is autoloaded, but you can
+    ;; optionally set a timer, which might speed up things as the
+    ;; codeium local language server takes ~0.2s to start up
+    ;; (add-hook 'emacs-startup-hook
+    ;;  (lambda () (run-with-timer 0.1 nil #'codeium-init)))
 
-(use-package codegpt
-  ;; Emacs code extension to generate or check code in the official OpenAI API
-  ;; [[https://github.com/emacs-openai/codegpt][codegpt]]
-  :straight (codegpt :type git :host github :repo "emacs-openai/codegpt")
-  :commands codegpt codegpt-custom codegpt-doc codegpt-fix codegpt-expain codegpt-improve)
+    ;; :defer t ;; lazy loading, if you want
+    :config
+    (setq use-dialog-box nil) ;; do not use popup boxes
 
+    ;; if you don't want to use customize to save the api-key
+    ;; (setq codeium/metadata/api_key "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
+
+    ;; get codeium status in the modeline
+    (setq codeium-mode-line-enable
+        (lambda (api) (not (memq api '(CancelRequest Heartbeat AcceptCompletion)))))
+    (add-to-list 'mode-line-format '(:eval (car-safe codeium-mode-line)) t)
+    ;; alternatively for a more extensive mode-line
+    ;; (add-to-list 'mode-line-format '(-50 "" codeium-mode-line) t)
+
+    ;; use M-x codeium-diagnose to see apis/fields that would be sent to the local language server
+    (setq codeium-api-enabled
+        (lambda (api)
+            (memq api '(GetCompletions Heartbeat CancelRequest GetAuthToken RegisterUser auth-redirect AcceptCompletion))))
+    ;; you can also set a config for a single buffer like this:
+    ;; (add-hook 'python-mode-hook
+    ;;     (lambda ()
+    ;;         (setq-local codeium/editor_options/tab_size 4)))
+
+    ;; You can overwrite all the codeium configs!
+    ;; for example, we recommend limiting the string sent to codeium for better performance
+    (defun my-codeium/document/text ()
+        (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (min (+ (point) 1000) (point-max))))
+    ;; if you change the text, you should also change the cursor_offset
+    ;; warning: this is measured by UTF-8 encoded bytes
+    (defun my-codeium/document/cursor_offset ()
+        (codeium-utf8-byte-length
+            (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (point))))
+    (setq codeium/document/text 'my-codeium/document/text)
+    (setq codeium/document/cursor_offset 'my-codeium/document/cursor_offset))
 
 ;;; init.el --- end
 (message "init.el ends here")
