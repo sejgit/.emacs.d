9001900bde17db73186487929537bc957f89c610
sejgit <5984155+sejgit@users.noreply.github.com>
2025-06-01 18:36:44 -0400

    add keyboard-quit-dwim

--- init.el
+++ init.el
@@ -952,6 +952,32 @@ Return its absolute path.  Otherwise, return nil."
                    (setq display-line-numbers-type 'relative)
                    (display-line-numbers-mode 1))))
 
+(defun sej/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+(global-set-key [remap keyboard-quit] #'sej/keyboard-quit-dwim)
+
 ;;;;; list-environment
 ;; environment variables tabulated
 ;; process environment editor
