7fdfbd43f378637aa87a56b89c889c8229cacc33
Stephen Jenkins <sejgit@github.com>
2025-11-21 07:57:11 -0500

    debug

--- recompile-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Recompile Emacs packages to suppress obsolete warnings
-
-emacs --batch --eval '
-(progn
-  (setq byte-compile-warnings (quote (not obsolete make-local suspicious lexical free-vars)))
-  (setq byte-compile-verbose nil)
-  (byte-recompile-directory (expand-file-name "~/.emacs.d/elpa") 0 t))
-'
